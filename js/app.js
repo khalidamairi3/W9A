08adc59b9e974e06b81835c390034988542d8168
khalidamairi3 <khalidamairi3@gmail.com>
2020-08-20 18:02:58 -0600

    W9B

--- js/app.js
+++ js/app.js
@@ -1,34 +1,49 @@
 var age = 25;
 var is_subscribed=true;
 
+var userAges = [17,20,40,15,22];
+var userSubscribtion = [true,false,false,true,true];
+var userNumber;
 
-if ( age < 18 ){
-    console.log("the user is younger than 18");
-}
-else{
-    console.log("the user is not younger than 18");
+for (var index=0; index < userAges.length ; index++){
+    userNumber=index+1
+
+    console.log("user number " + userNumber + ":");
+
+    if ( userAges[index] < 18 ){
+        console.log("the user  is younger than 18");
+    }
+    else{
+        console.log("the user is not younger than 18");
+    }
+    
+    if (userSubscribtion[index]){
+        console.log("the user is subscribed");
+    }
+    else{
+        console.log("the user is not subscribed");
+    }
+    
+    if (userAges[index] < 18 && !userSubscribtion[index]){
+        console.log("the user is younger than 18 and not subscribed");
+    }
+    if (userAges[index] > 18 && !userSubscribtion[index]){
+        console.log("the user is older than 18 and not subscribed");
+    }
+    if(userAges[index] < 18 && userSubscribtion[index]){
+        console.log("the user is younger than 18 and is subscribed");
+    
+    }
+    if( userAges[index] > 18 && userSubscribtion[index]){
+        console.log("the user is older than 18 and is subscribed");
+    }
+
+
+
+
 }
 
-if (is_subscribed==true){
-    console.log("the user is subscribed");
-}
-else{
-    console.log("the user is not subscribed");
-}
 
-if (age < 18 && is_subscribed==false){
-    console.log("the user is younger than 18 and not subscribed");
-}
-if (age > 18 && is_subscribed==false){
-    console.log("the user is older than 18 and not subscribed");
-}
-if(age < 18 && is_subscribed==true){
-    console.log("the user is younger than 18 and is subscribed");
-
-}
-if( age > 18 && is_subscribed==true){
-    console.log("the user is older than 18 and is subscribed");
-}
 
 
  
